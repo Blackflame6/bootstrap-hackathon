2d1bc15e601049ae1565cb5395cba19ef58c0e00
Blackflame6 <engineerfranklyn@gmail.com>
2022-08-10 08:27:05 -0500

    updated website images

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-const button = document.querySelector('button')
-validate()
-function validate () {
-  let username = document.getElementById('username').nodeValue;
+const button = getElementById('signing')
+let username = document.getElementById('username').Value;
   let password = document.getElementById('password').value
+
+function validate () {
+  
 button.addEventListener ('click', login)
 
 }
 function login() {
 
   if (username == 'franklyn' && password == 'user') {
-    alert(`${username} login sucessful`)
+    alert("login sucessful")
   } else {
-    alert (`${username} login failed`)
+    alert ("login failed")
   }
 }
